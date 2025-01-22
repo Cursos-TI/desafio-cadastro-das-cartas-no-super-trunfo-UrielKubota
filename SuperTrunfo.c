2ab06c08da0f1423ab57fb9fb646028dba2b614e
UrielKubota <uriback2011@hotmail.com>
2025-01-22 15:39:44 +0000

    Carta Pais Criada

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -2,10 +2,26 @@
 
 int main (){
 // Atributos das cartas
-int populacao;
-float area;
-float PIB;
-int pontosturisticos;
+    float populacao;
+    double area;
+    long PIB;
+    int pontosturisticos;
+    char nome [50];
+//Cartas
+    printf ("Pais\n");
+    printf ("Nome:\n");
+    scanf ("%s", &nome);
+    printf ("População do Brasil:\n");
+    scanf ("%f", &populacao); 
+    printf ("Area do Brasil:\n");
+    scanf ("%Lf", &area); 
+    printf ("PIB do Brasil:\n");
+    scanf ("%lf", &PIB); 
+    printf ("Pontos Turisticos do Brasil:\n");
+    scanf ("%d", &pontosturisticos); 
+
+    printf ("Nome: %s População do Brasil: %f milhoes Area: %f km\n", nome, populacao, area);
+    printf ("PIB: %f trilhoes Pontos Turisticos: %d\n",PIB, pontosturisticos);
 
 
 
